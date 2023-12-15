d21cbbd6728b61c054e49e405677e65692bd2cf1
cmdoret <cyril.mattheydoret@gmail.com>
2023-12-15 16:26:34 +0100

    add placeholder cram api

--- modo/cram.py
+++ modo/cram.py
@@ -15,7 +15,20 @@ def slice(cram_path: AlignmentFile, coords: str) -> AlignmentFile:
 
 
 def extract_metadata(AlignmentHeader) -> Graph:
+    """Extract metadata from the CRAM file header and
+    convert specific attributes to an RDF graph according
+    to the modo schema."""
+    # NOTE: Not a priority
     ...
+
+
+def validata_cram_files(cram_path: str) -> bool:
+    """Validate CRAM files using pysam.
+    Checks if the file is sorted and has an index."""
+    # TODO:
+    # Check if sorted
+    # Check if index exists
+    # Check if reference exists
 
 
 # TODO: Add functions to edit CRAM files (liftover)
