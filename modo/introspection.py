134617425b2e25e345211fec8635e994a515a663
cmdoret <cyril.mattheydoret@gmail.com>
2024-01-11 13:43:22 +0100

    docs: mention class-independence in get_slot_range docstring

--- modo/introspection.py
+++ modo/introspection.py
@@ -59,9 +59,7 @@ def instance_to_graph(instance) -> Graph:
 
 
 def get_slot_range(slot_name: str) -> str:
-    """Return the range of a slot."""
-    # NOTE: This assumes slot range is class-independent and
-    # will not work if the schema uses linkml's slot_usage feature
+    """Return the class-independent range of a slot."""
     return load_schema().get_slot(slot_name).range
 
 
