f78a6e4347108903569ca2415b306177c741f7db
almutlue <almut.lue@gmail.com>
2024-02-07 08:35:43 +0100

    feat(api): update existing elements

--- modo/api.py
+++ modo/api.py
@@ -14,7 +14,7 @@ from .introspection import get_haspart_property
 from .rdf import attrs_to_graph
 from .storage import add_metadata_group, init_zarr, list_zarr_items
 from .file_utils import extract_metadata
-from .helpers import dict_to_instance
+from .helpers import dict_to_instance, class_from_name
 
 
 class MODO:
@@ -206,6 +206,29 @@ class MODO:
         add_metadata_group(parent_group, attrs)
         zarr.consolidate_metadata(self.archive.store)
 
+    def update_element(
+        self,
+        element_id: str,
+        new: model.DataEntity | model.Sample | model.Assay | model.MODO,
+    ):
+        """Update element by adding new values from model object"""
+        attrs = self.archive[element_id].attrs
+        attr_dict = attrs.asdict()
+        if not isinstance(new, class_from_name(attr_dict.get("@type"))):
+            raise ValueError(
+                f"Class {attr_dict['@type']} of {element_id} does not match {new.class_name}."
+            )
+        new_items = {
+            field: value
+            for field, value in new._items()
+            if field not in attrs.keys()
+            and field != "id"
+            and value is not None
+            or isinstance(value, list)
+            and len(value) == 0
+        }
+        attrs.update(**new_items)
+
     def extract_metadata(self):
         """Add metadata and corresponding elements extracted from object associated data to the MODO object"""
         new_elements = []
@@ -213,14 +236,18 @@ class MODO:
             dict_to_instance(entity, id)
             for id, entity in self.metadata.items()
         ]
-        inst_names = [inst.name for inst in instances]
+        inst_names = {inst.name: inst.id for inst in instances}
         for inst in instances:
             elements = extract_metadata(inst)
-            new_elements += [
-                ele
-                for ele in elements
+            for ele in elements:
                 # NOTE: Need to compare names here as ids differ
-                if ele.name not in inst_names and ele not in new_elements
-            ]
-        for ele in new_elements:
-            self.add_element(ele)
+                if (
+                    ele.name not in inst_names.keys()
+                    and ele not in new_elements
+                ):
+                    new_elements.append(ele)
+                    self.add_element(ele)
+                elif ele.name in inst_names.keys():
+                    self.update_element(inst_names[ele.name], ele)
+                else:
+                    continue
