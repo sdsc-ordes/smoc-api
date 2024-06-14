3fc2bf05e9641e7c557455f5d6002006fb5b0a2e
cmdoret <cyril.mattheydoret@gmail.com>
2024-06-14 17:54:09 +0200

    refactor(storage): specify attributes in abc via properties

--- modos/storage.py
+++ modos/storage.py
@@ -1,52 +1,75 @@
 from abc import ABC, abstractmethod
 from pathlib import Path
 import shutil
-from typing import Any, Optional
+from typing import Any, Generator, Optional
 
 import s3fs
 import zarr
+import zarr.hierarchy as zh
 
 
-from .helpers import ElementType
+from .helpers import ElementType, GenomicFileSuffix
 
-ZARR_ROOT = "data.zarr"
+ZARR_ROOT = Path("data.zarr")
 
 
 class Storage(ABC):
+    @property
     @abstractmethod
-    def exists(self, path: str) -> bool:
+    def path(self) -> Path:
+        ...
+
+    @property
+    @abstractmethod
+    def zarr(self) -> zh.Group:
         ...
 
     @abstractmethod
-    def put(self, source: str, target: str):
+    def exists(self, target: Path) -> bool:
         ...
 
     @abstractmethod
-    def remove(path: str):
+    def put(self, source: Path, target: Path):
         ...
 
     @abstractmethod
-    def list(path: str):
+    def remove(self, target: Path):
         ...
+
+    @abstractmethod
+    def list(self, target: Optional[Path]) -> Generator[Path, None, None]:
+        ...
+
+    def empty(self) -> bool:
+        return len(self.zarr.attrs.keys()) == 0
 
 
 class LocalStorage(Storage):
     def __init__(self, path: Path):
-        self.path = Path(path)
+        self._path = Path(path)
         if (self.path / ZARR_ROOT).exists():
-            self.zarr = zarr.convenience.open(str(self.path / ZARR_ROOT))
+            self._zarr = zarr.convenience.open(str(self.path / ZARR_ROOT))
         else:
             self.path.mkdir(exist_ok=True)
             zarr_store = zarr.storage.DirectoryStore(
                 str(self.path / ZARR_ROOT)
             )
-            self.zarr = init_zarr(zarr_store)
+            self._zarr = init_zarr(zarr_store)
 
-    def exists(self, path: str = ZARR_ROOT) -> bool:
-        return (self.path / path).exists()
+    @property
+    def zarr(self) -> zh.Group:
+        return self._zarr
 
-    def list(self):
-        for path in self.path.glob("*"):
+    @property
+    def path(self) -> Path:
+        return self._path
+
+    def exists(self, target: Path) -> bool:
+        return (self.path / target).exists()
+
+    def list(self, target: Optional[Path] = None):
+        path = self.path / (target or "")
+        for path in path.glob("*"):
             if path.name.endswith(".zarr"):
                 continue
             elif path.is_file():
@@ -55,13 +78,14 @@ class LocalStorage(Storage):
                 if file.is_file():
                     yield file
 
-    def remove(self, data_file: Path):
-        if data_file.exists():
-            data_file.unlink()
-            print(f"INFO: Permanently deleted {data_file} from filesystem.")
+    def remove(self, target: Path):
+        path = self.path / target
+        if path.exists():
+            path.unlink()
+            print(f"INFO: Permanently deleted {target} from filesystem.")
 
-    def put(self, source: Path, dest: Path):
-        shutil.copy(source, self.path / dest)
+    def put(self, source: Path, target: Path):
+        shutil.copy(source, self.path / target)
 
 
 class S3Storage(Storage):
@@ -71,14 +95,14 @@ class S3Storage(Storage):
         s3_endpoint: dict[str, Any],
         s3_kwargs: dict[str, Any],
     ):
-        self.path = Path(path)
+        self._path = Path(path)
         self.endpoint = s3_endpoint
         s3_opts = s3_kwargs or {"anon": True}
         fs = s3fs.S3FileSystem(endpoint_url=s3_endpoint, **s3_opts)
         if fs.exists(str(self.path / ZARR_ROOT)):
             zarr_s3_opts = s3_opts | {"endpoint_url": s3_endpoint}
 
-            self.zarr = zarr.convenience.open(
+            self._zarr = zarr.convenience.open(
                 f"s3://{path}/{ZARR_ROOT}",
                 storage_options=zarr_s3_opts,
             )
@@ -87,38 +111,49 @@ class S3Storage(Storage):
             zarr_store = zarr.storage.FSStore(
                 str(self.path / ZARR_ROOT), fs=fs
             )
-            self.zarr = init_zarr(zarr_store)
+            self._zarr = init_zarr(zarr_store)
 
-    def exists(self, path: str = ZARR_ROOT) -> bool:
-        fs = self.zarr.store.fs
-        return fs.exists(str(self.path / path))
+    @property
+    def path(self) -> Path:
+        return self._path
 
-    def list(self):
+    @property
+    def zarr(self) -> zh.Group:
+        return self._zarr
+
+    def exists(self, target: Path = ZARR_ROOT) -> bool:
         fs = self.zarr.store.fs
-        for path in fs.glob(f"{self.path}/*"):
-            if Path(path).name.endswith(".zarr"):
+        return fs.exists(str(self.path / target))
+
+    def list(
+        self, target: Optional[Path] = None
+    ) -> Generator[Path, None, None]:
+        fs = self.zarr.store.fs
+        path = self.path / (target or "")
+        for node in fs.glob(f"{path}/*"):
+            if Path(node).name.endswith(".zarr"):
                 continue
-            elif fs.isfile(path):
-                yield Path(path)
-            elif fs.isdir(path):
-                for file in fs.find(path):
+            elif fs.isfile(node):
+                yield Path(node)
+            elif fs.isdir(node):
+                for file in fs.find(node):
                     yield Path(file)
 
-    def remove(self, data_file: Path):
-        if self.zarr.store.fs.exists(data_file):
-            self.zarr.store.fs.rm(str(data_file))
+    def remove(self, target: Path):
+        if self.zarr.store.fs.exists(target):
+            self.zarr.store.fs.rm(str(target))
             print(
-                f"INFO: Permanently deleted {data_file} from remote filesystem."
+                f"INFO: Permanently deleted {target} from remote filesystem."
             )
 
-    def put(self, source: Path, dest: Path):
-        self.zarr.store.fs.put_file(source, self.path / Path(dest))
+    def put(self, source: Path, target: Path):
+        self.zarr.store.fs.put_file(source, self.path / Path(target))
 
 
 # Initialize object's directory given the metadata graph
-def init_zarr(zarr_store: zarr.storage.Store) -> zarr.hierarchy.Group:
+def init_zarr(zarr_store: zarr.storage.Store) -> zh.Group:
     """Initialize object's directory and metadata structure."""
-    data = zarr.hierarchy.group(store=zarr_store)
+    data = zh.group(store=zarr_store)
     elem_types = [t.value for t in ElementType]
     for elem_type in elem_types:
         data.create_group(elem_type)
@@ -126,9 +161,7 @@ def init_zarr(zarr_store: zarr.storage.Store) -> zarr.hierarchy.Group:
     return data
 
 
-def add_metadata_group(
-    parent_group: zarr.hierarchy.Group, metadata: dict
-) -> None:
+def add_metadata_group(parent_group: zh.Group, metadata: dict) -> None:
     """Add input metadata dictionary to an existing zarr group."""
     # zarr groups cannot have slashes in their names
     group_name = metadata["id"].replace("/", "_")
@@ -140,14 +173,14 @@ def add_metadata_group(
         parent_group[group_name].attrs[key] = value
 
 
-def add_data(group: zarr.hierarchy.Group, data) -> None:
+def add_data(group: zh.Group, data) -> None:
     """Add a numpy array to an existing zarr group."""
     group.create_dataset("data", data=data)
 
 
 def list_zarr_items(
-    group: zarr.hierarchy.Group,
-) -> list[zarr.hierarchy.Group | zarr.core.Array]:
+    group: zh.Group,
+) -> list[zh.Group | zarr.core.Array]:
     """Recursively list all zarr groups and arrays"""
     found = []
 
