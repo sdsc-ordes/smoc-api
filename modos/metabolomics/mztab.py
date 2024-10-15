61762e3d6a90a529b93ea29bc8fc65d2553d9d8d
cmdoret <cyril.mattheydoret@gmail.com>
2024-10-15 15:30:30 +0200

    feat(mztab): end-to-end mztab metadata extraction

--- modos/metabolomics/mztab.py
+++ modos/metabolomics/mztab.py
@@ -1,10 +1,13 @@
 """Module for extracting metabolomics metadata from mzTab-M 2.0 files."""
+from pathlib import Path
+import re
+
 from pyteomics.mztab import MzTab
 import modos_schema.datamodel as model
 
 
-def load_mztab(path: str) -> MzTab:
-    return MzTab(path)
+def load_mztab(path: Path) -> MzTab:
+    return MzTab(str(path))
 
 
 def get_samples(mz: MzTab) -> list[model.Sample]:
@@ -22,14 +25,25 @@ def get_samples(mz: MzTab) -> list[model.Sample]:
     return samples
 
 
-def get_assay(mz: MzTab) -> list[model.Assay]:
+def get_assay(mz: MzTab) -> model.Assay:
+    meta = mz.metadata
     assay = model.Assay(
-        id=f"assay/{mz.id}",
-        name=mz.title,
-        description=mz.description,
-        has_data=f"data/{mz.id}",
+        id=f"assay/{meta['mzTab-ID']}",
+        name=meta.get("title", None),
+        description=meta.get("description", None),
+        has_data=f"data/{meta['mzTab-ID']}",
         has_sample=[sample["name"] for sample in mz.samples.values()],
-        omics_type="METABOLOMICS",
-        sample_processing=mz.sample_processing,
+        omics_type="METABOLOMICS"
+        if re.match(r".*-M", mz.version)
+        else "PROTEOMICS",
+        sample_processing=meta.get("sample_processing", None),
     )
     return assay
+
+
+def extract_mztab_metadata(path: Path) -> list[model.Assay | model.Sample]:
+    mz = load_mztab(path)
+    elems = []
+    elems.extend(get_samples(mz))
+    elems.append(get_assay(mz))
+    return elems
